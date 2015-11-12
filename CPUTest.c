e6ec0ec5d5637e84e8eeea0758bc03998f4f2656
priyesh <priyesh16@gmail.com>
2015-11-12 01:08:35 -0800

    cpushare initial

--- CPUTest.c
+++ CPUTest.c
@@ -4,11 +4,16 @@
 #include <stdio.h>
 
 #define MILLION 1000000
+#define SCHEDOFF 3 
 int main(int argc, char **argv) {
 	long int iterations = MILLION;
 	struct timeval start, end;
 	long int i;
 	long int time;
+	long int prevtime = 0;
+	long int offcount = 0;
+	long int oncount = 0;
+	long int inc = 0;
 
 	gettimeofday(&start, NULL);
 
@@ -16,7 +21,12 @@ int main(int argc, char **argv) {
 	   gettimeofday(&end, NULL);
 	   time = (end.tv_sec * 1000000 + end.tv_usec) -
 		    (start.tv_sec * 1000000 + start.tv_usec);
-	   printf("%ld\t%ld\n", i, time); 
+	   if ((time - prevtime) > SCHEDOFF) {
+			inc = (time - prevtime)/SCHEDOFF;
+			offcount = offcount + inc;	
+	   }
+	   printf("%ld\t%ld\t%ld\n", i, time, offcount); 
+	   prevtime = time;
 	}
 	return 0;
 }
